aff0ec6234865a51ab61d88416e4404d0e5c7bac
Yatogaii <781023956@qq.com>
2023-11-09 15:04:51 +0800

    添加函数返回值为函数指针并立刻调用的情况（嵌套CallInst），通过 test11.ll

--- LLVMAssignment.cpp
+++ LLVMAssignment.cpp
@@ -119,15 +119,98 @@ struct FuncPtrPass : public ModulePass {
     }
   }
 
+  /// test04.ll : i32 (i32, i32)* %a_fptr 是参数类型
+  /// 需要添加处理函数参数的代码
+  /// test11.ll ： %call = call i32 (i32, i32)* @foo(i32 %a, i32 %b, i32 (i32, i32)* %a_fptr, i32 (i32, i32)* %b_fptr), !dbg !19
+  /// 需要考虑函数返回值为函数的情况
   void handleValue(const Value* value, int line){
       if(auto* phiNode = dyn_cast<PHINode>(value)){
           handlePHINode(phiNode, line);
       } else if(auto* argument = dyn_cast<Argument>(value)) {
           handleArgument(argument, line);
-      }else {
-          /// test04.ll : i32 (i32, i32)* %a_fptr 是参数类型
+      } else if(auto* call = dyn_cast<CallInst>(value)) {
+          /// test11.ll 主要修改这个 if 里的逻辑
+          /// 需要支持函数返回值为函数指针的情况
+          // Check if the call returns a pointer to a function type
+          if (call->getType()->isPointerTy() &&
+              call->getType()->getPointerElementType()->isFunctionTy()) {
+              handleFuncPtrRet(call, line);
+          } else {
+              // Otherwise, it's a regular function call
+              handleCall(call, line);
+          }
+      } else if(auto* func = dyn_cast<Function>(value)) {
+          handleFunc(func, line);
+      //} else if(auto* ret = dyn_cast<ReturnInst>(value)) {
+     //     handleRet(ret, line);
+      } else {
           errs() << "Unsupport value: ";
           value->dump();
+      }
+  }
+
+  /// test11.ll 函数指针为一个函数返回值的情况，否则如果进入到 handleCall 会获得错误结果。
+  /// 为什么不用遍历 User 来处理，而是遍历嵌套 CallInst ：
+  /// 函数指针被返回并立即用于另一个 CallInst，分析应该跟踪到这个立即的 CallInst。
+  void handleFuncPtrRet(const CallInst* call, int line){
+      if (const Function *calledFunc = call->getCalledFunction()) {
+          // CallInst 的内部嵌套 CallInst，说明实际调用的是 Inner CallInst
+          // 的返回值，而不是 Inner CallInst 自身。
+          for (const BasicBlock &bb : *calledFunc) {
+              for (const Instruction &i : bb) {
+                  if (const ReturnInst *retInst = dyn_cast<ReturnInst>(&i)) {
+                      const Value *retValue = retInst->getReturnValue();
+                      handleValue(retValue, line);
+                  }
+              }
+          }
+      }
+      return ;
+
+      // Iterate over all the users of this function pointer
+      for (auto* user : call->users()) {
+          if (isa<CallInst>(user)) {
+              if(auto* userCallInst = dyn_cast<CallInst>(user)) {
+                  // If the user is a CallInst, it may be a function call using the pointer
+                  handleCall(userCallInst, line);
+              }
+//          } else if (auto* storeInst = dyn_cast<StoreInst>(user)) {
+//              // If the user is a StoreInst, the function pointer is stored somewhere
+//              const Value* storedValue = storeInst->getValueOperand();
+//              // Continue to track where this stored value is used
+//              trackStoredFunctionPointer(storedValue, line);
+          }
+          // ... handle other types of users
+      }
+  }
+
+
+
+  void handleFunc(const Function* func, int line){
+      // Ignore intrinsic functions, return true when function start with llvm
+      if (!func->isIntrinsic()) {
+          // 这里获取不到在 if 里面赋值的函数指针，需要处理 PHINode
+          lineToFunctionsMap[line].insert(func->getName().str());
+      }
+  }
+
+  // 独立出 handleCall 处理 call 前套的问题。
+  /// test11.ll 这里 handleCall 会输出错误的结果，猜测是返回值的问题
+  /// handleCall -> handleValue -> handleCall
+  void handleCall(const CallInst* call, int line){
+      // 可以直接换成 Function 的
+      if (Function *calledFunction = call->getCalledFunction()) {
+          handleFunc(calledFunction, line);
+      } else { // 不可以直接换成 Function 的
+          // 获取操作数
+          /// test11.ll 流程：
+          /// 1. 先获取 Call，发现不是直接调用
+          /// 2. 把 Operand 传递给了 handleValue
+          /// 3. handleValue 发现 Operand 是一个直接调用
+          /// 4. 记录答案
+          /// 错在了 Operand 虽然是一个直接调用，但是调用的是 foo 返回的函数指针 plus。
+          const Value *value = call->getCalledOperand();
+          handleValue(value, line);
       }
   }
 
@@ -142,30 +225,7 @@ struct FuncPtrPass : public ModulePass {
                   //errs() << cnt ++;
                   //I.dump();
                   if (auto *callInst = dyn_cast<CallInst>(&I)) {
-                      // 可以直接换成 Function 的
-                      if (Function *calledFunction = callInst->getCalledFunction()) {
-                          // Ignore intrinsic functions, return true when function start with llvm
-                           if (!calledFunction->isIntrinsic()) {
-                              // 这里获取不到在 if 里面赋值的函数指针，需要处理 PHINode
-                              if (const DebugLoc &debugInfo = I.getDebugLoc()) { // Here the debug information is obtained
-                                  lineToFunctionsMap[LINE].insert(calledFunction->getName().str());
-                              }
-                           }
-                      } else { // 不可以直接换成 Function 的
-                          // 获取操作数
-                          const Value *value = callInst->getCalledOperand();
-                          handleValue(value, LINE);
-//                          if(auto* phiNode = dyn_cast<PHINode>(value)){
-//                              handlePHINode(phiNode, I.getDebugLoc().getLine());
-//                          } else if(auto* argument = dyn_cast<Argument>(value)) {
-//                              handleArgument(argument, I.getDebugLoc().getLine());
-//                          }else {
-//                              /// test04.ll : i32 (i32, i32)* %a_fptr 是参数类型
-//                              errs() << "Unsupport format : ";
-//                              value->dump();
-//                          }
-
-                      }
+                      handleCall(callInst, I.getDebugLoc().getLine());
                   }
               }
           }
