2ba1bd15993b20730d9a22d2cb06343a6a4ebeb1
Yatogaii <781023956@qq.com>
2023-11-14 10:46:05 +0800

    添加了isIndirect标志位防止handleFunc提前添加结果，通过测试用例17

--- LLVMAssignment.cpp
+++ LLVMAssignment.cpp
@@ -69,6 +69,8 @@ struct FuncPtrPass : public ModulePass {
   static char ID; // Pass identification, replacement for typeid
   FuncPtrPass() : ModulePass(ID) {}
 
+  bool isIndirect = false;
+
   std::map<unsigned, std::set<std::string>> lineToFunctionsMap;
 
     /// PHINode 处理函数，可能有递归
@@ -78,30 +80,6 @@ struct FuncPtrPass : public ModulePass {
       for (unsigned i = 0; i < phiNode->getNumIncomingValues(); ++i) {
           Value *incomingValue = phiNode->getIncomingValue(i);
           handleValue(incomingValue, line);
-          continue;
-            // ---------------------------------
-          // 处理每个前驱值
-          if (auto *incomingPHINode = dyn_cast<PHINode>(incomingValue)) {
-              // 如果前驱值是另一个PHINode，递归处理
-              handlePHINode(incomingPHINode, line);
-//          } else if (auto *inst = dyn_cast<Instruction>(incomingValue)) {
-//              // 如果前驱值是一个指令，处理这个指令
-//              // ...
-//          } else if (auto *arg = dyn_cast<Argument>(incomingValue)) {
-//              // 如果前驱值是一个参数，处理这个参数
-//              // ...
-          } else if (auto *func = dyn_cast<Function>(incomingValue)){
-              // 前驱是一个函数
-              lineToFunctionsMap[line].insert(func->getName());
-          } else if (auto *arg = dyn_cast<Argument>(incomingValue)) {
-              handleArgument(arg, line);
-          } else if (auto *call = dyn_cast<CallInst>(incomingValue)) {
-              handleArgument(arg, line);
-          } else {
-              /// test10.ll :%s_fptr.0 = phi i32 (i32, i32)* [ %a_fptr, %if.then ], [ %b_fptr, %if.else ], !dbg !24
-              errs() << "Unable handler PHINode :" << "\n";
-              phiNode->dump();
-          }
       }
   }
 
@@ -119,8 +97,40 @@ struct FuncPtrPass : public ModulePass {
     for(const User* user: parent->users()){
         // 获取参数所在函数的调用
         if (const CallInst *callInst = dyn_cast<CallInst>(user)) {
-            Value *operand = callInst->getArgOperand(argIdx);
-            handleValue(operand, line);
+            user->dump();
+            // pasted from https://github.com/ChinaNuke/llvm-pass/blob/master/LLVMAssignment.cpp
+            const Function *calledFunction = callInst->getCalledFunction();
+            if (calledFunction == parent) {
+                const Value *operand = callInst->getArgOperand(argIdx);
+                /// test17.ll 这里会直接跳到handleValue之后handleFunc
+                //if(auto* func = dyn_cast<Function>(operand)){
+                    // 这里还需要额外操作。
+
+                //} else {
+                    handleValue(operand,line);
+                //}
+            } else {
+                for (const BasicBlock &bb : *calledFunction) {
+                    for (const Instruction &i : bb) {
+                        if (const ReturnInst *retInst = dyn_cast<ReturnInst>(&i)) {
+                            const Value *retValue = retInst->getReturnValue();
+                            if (const Argument *arg = dyn_cast<Argument>(retValue)) {
+                                handleArgument(arg, line);
+                            } else if (const CallInst *callInst = dyn_cast<CallInst>(retValue)) {
+                                const Value *operand = callInst->getArgOperand(argIdx);
+                                if (const Argument *arg = dyn_cast<Argument>(operand)) {
+                                    handleArgument(arg, line);
+                                } else {
+                                }
+                            } else {
+                            }
+
+                        }
+                    }
+                }
+            }
+//            Value *operand = callInst->getArgOperand(argIdx);
+//            handleValue(operand, line);
         } else if (const PHINode *phiNode = dyn_cast<PHINode>(user)) {
             for (const User *phiUser: phiNode->users()) {
                 if (const CallInst *outerCallInst = dyn_cast<CallInst>(phiUser)) {
@@ -148,14 +158,14 @@ struct FuncPtrPass : public ModulePass {
           /// test11.ll 主要修改这个 if 里的逻辑
           /// 需要支持函数返回值为函数指针的情况
           /// 比如 ret i32 (i32, i32)* %call, !dbg !20 里 getReturnVal 获得的就是 %call 他是一个函数指针
-          // Check if the call returns a pointer to a function type
-          if (call->getType()->isPointerTy() &&
-              call->getType()->getPointerElementType()->isFunctionTy()) {
-              handleFuncPtrRet(call, line);
-          } else {
-              // Otherwise, it's a regular function call
-              handleCall(call, line);
-          }
+          /// 不应该放在 handleValue 里做
+//          if (call->getType()->isPointerTy() &&
+//              call->getType()->getPointerElementType()->isFunctionTy()) {
+//              handleFuncPtrRet(call, line);
+//          } else {
+//              handleCall(call, line);
+//          }
+          handleCall(call, line);
       } else if (const ReturnInst *returnInst = dyn_cast<ReturnInst>(value)) {
           handleReturn(returnInst, line);
       } else if(auto* func = dyn_cast<Function>(value)) {
@@ -173,7 +183,9 @@ struct FuncPtrPass : public ModulePass {
   /// 函数指针被返回并立即用于另一个 CallInst，分析应该跟踪到这个立即的 CallInst。
   /// test16.ll should output minus but get plus
   /// 我怎么觉得就是 plus 呢，答案是不是错了
+  /// 该函数整合到 handleCall 里比较合适，因为他关注的是 %call 这类的指针而不是具体的return语句
   void handleFuncPtrRet(const CallInst* call, int line){
+      const Value* operand = call->getCalledOperand();
       if (const Function *calledFunc = call->getCalledFunction()) {
           // CallInst 的内部嵌套 CallInst，说明实际调用的是 Inner CallInst
           // 的返回值，而不是 Inner CallInst 自身。
@@ -190,9 +202,21 @@ struct FuncPtrPass : public ModulePass {
                   }
               }
           }
+      } else if(const CallInst* innerCall = dyn_cast<CallInst>(operand)){
+          /// test17.ll 多层嵌套, 没有触发
+          errs() << "多层嵌套触发";
+      } else {
+          /// test17.ll
+          /// %call = call i32 (i32, i32)* %goo_ptr(i32 %a, i32 %b, i32 (i32, i32)* %b_fptr, i32 (i32, i32)* %a_fptr), !dbg !23
+          errs() << "handleFuncPtrRet unhandled:" ;
+          call->dump();
+          errs() << "Operand: ";
+          operand->dump();
+          // test17.ll 在这里分析 argument ?
+          handleValue(operand, line);
       }
-      return ;
 
+      return ;
       // Iterate over all the users of this function pointer
       for (auto* user : call->users()) {
           if (isa<CallInst>(user)) {
@@ -216,7 +240,27 @@ struct FuncPtrPass : public ModulePass {
       // Ignore intrinsic functions, return true when function start with llvm
       if (!func->isIntrinsic()) {
           // 这里获取不到在 if 里面赋值的函数指针，需要处理 PHINode
-          lineToFunctionsMap[line].insert(func->getName().str());
+          func->dump();
+          /// 这里不能直接加入到结果集，他有可能执行的是返回的函数？
+          auto* type = func->getReturnType();
+          if(isIndirect && type->isPointerTy() && type->getPointerElementType()->isFunctionTy()){
+              /// test17.ll
+              /// 这样写会导致结果全部变成了plus，但是25行的实际调用确实是plus
+              /// 需要做一个额外区分。
+              for (const BasicBlock &bb : *func) {
+                  for (const Instruction &i : bb) {
+                      if (const ReturnInst *retInst = dyn_cast<ReturnInst>(&i)) {
+                          const Value *retValue = retInst->getReturnValue();
+                          errs() << "handled funcPtrRet:::";
+                          i.dump();
+                          handleValue(retValue, line);
+                      }
+                  }
+              }
+          } else {
+              lineToFunctionsMap[line].insert(func->getName().str());
+          }
+          isIndirect = false;
           /// test13.ll call 里面又有其他的call，比如
           /// %call = call i32 (i32, i32)* %2(i32 %3, i32 %4, i32 (i32, i32)* %5, i32 (i32, i32)* %6), !dbg !86
       }
@@ -228,6 +272,8 @@ struct FuncPtrPass : public ModulePass {
           for (const Instruction &i : bb) {
               if (const ReturnInst *retInst = dyn_cast<ReturnInst>(&i)) {
                   const Value *retValue = retInst->getReturnValue();
+                  errs() << "handle Ret: ";
+                  retInst->dump();
                   handleValue(retValue, line);
               }
           }
@@ -255,18 +301,25 @@ struct FuncPtrPass : public ModulePass {
           /// handleValue 识别不了嵌套CallInst 的情况。
           /// 这里调用 handleValue 无限递归。
           const Value *operand = call->getCalledOperand();
+          /// call的 operand 仍然是一个 call，这里可以认为是调用了函数指针，需要分析函数指针的值。j
           if (const CallInst *innerCallInst = dyn_cast<CallInst>(operand)) {
+              isIndirect = true;
               /// test16.ll: 嵌套 Call 的情况:   %call1 = call i32 %call(i32 %op1, i32 %op2), !dbg !27
               errs() << "嵌套 Call 的情况: ";
               call->dump();
               errs() << "inner: ";
               innerCallInst->dump();
               errs() << "\n";
-              // CallInst 的内部嵌套 CallInst，说明实际调用的是 Inner CallInst
-              // 的返回值，而不是 Inner CallInst 自身。
+
+              /// 内部call可以直接获取函数
               if (const Function *calledFunc = innerCallInst->getCalledFunction()) {
+                  /// test17.ll inner:
+                  /// %call = call i32 (i32, i32)* @clever1(i32 (i32, i32)* (i32, i32, i32 (i32, i32)*, i32 (i32, i32)*)* @clever, i32 %op1, i32 %op2, i32 (i32, i32)* @minus, i32 (i32, i32)* @plus), !dbg !25
+                  /// test17.ll 34行的IR handleFunctionReturnStatement->handleValue->handleFuncPtrRet(这里直接if为false) 结束
+                  /// 实际上 test17.ll 不仅仅套了一层，而是套了很多层，所以需要在handleFuncPtrRet补全if else
                   handleFuntionReturnStatement(calledFunc, line);
-              } else { // 这里是 innerFunction 又是间接调用的情况
+              } else {
+                  // 这里是 innerCall 又是间接调用的情况
                   const Value *innerCallInstOperand = innerCallInst->getCalledOperand();
                   if (const PHINode *phiNode =
                           dyn_cast<PHINode>(innerCallInstOperand)) {
